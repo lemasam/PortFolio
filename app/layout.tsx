a0cc2f6909e2f13b1724210d797a816cc4612675
Catherine Vuthi <catherine@startupblink.com>
2024-08-14 22:25:53 +0300

    Change layouts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
-import "./globals.css";
+import "./ui/globals.css";
 
 const roboto = Roboto({
   weight: ['100', '300', '400', '500', '700'],
