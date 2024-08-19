87839e7a4fbcf9973f697e2d879ffba6793b9018
esthercate <vuthi.esther@gmail.com>
2024-08-19 21:56:40 +0300

    Add other resume components

--- app/components/Resume.tsx
+++ app/components/Resume.tsx
@@ -1,12 +1,33 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import { Card } from "./common/styles";
 import About from "./common/About";
+// Import other components like Education, Experience, ProfessionalSkills
+import Education from "./common/Education";
+import Experience from "./common/Experience";
+import ProfessionalSkills from "./common/ProfessionalSkills";
 
 type Props = {};
 
 const Resume = (props: Props) => {
+  const [activeSection, setActiveSection] = useState("about");
+
+  const renderContent = () => {
+    switch (activeSection) {
+      case "about":
+        return <About />;
+      case "education":
+        return <Education />;
+      case "experience":
+        return <Experience />;
+      case "skills":
+        return <ProfessionalSkills />;
+      default:
+        return null;
+    }
+  };
+
   return (
     <div
       className="bg-bg-dark py-10 tablet:py-28 px-5 tablet:px-16 flex flex-col gap-12"
@@ -20,14 +41,40 @@ const Resume = (props: Props) => {
       </div>
       <div className="flex">
         <div className="w-1/3 flex flex-col gap-8">
-          <Card className="w-3/4 px-4 py-3">About Me</Card>
-          <Card className="w-3/4 px-4 py-3 text-left">Education</Card>
-          <Card className="w-3/4 px-4 py-3">Experience</Card>
-          <Card className="w-3/4 px-4 py-3">Professional Skills</Card>
+          <Card
+            className={`w-3/4 px-4 py-3 ${
+              activeSection === "about" ? "bg-orange" : ""
+            }`}
+            onClick={() => setActiveSection("about")}
+          >
+            About Me
+          </Card>
+          <Card
+            className={`w-3/4 px-4 py-3 ${
+              activeSection === "education" ? "bg-orange" : ""
+            }`}
+            onClick={() => setActiveSection("education")}
+          >
+            Education
+          </Card>
+          <Card
+            className={`w-3/4 px-4 py-3 ${
+              activeSection === "experience" ? "bg-orange" : ""
+            }`}
+            onClick={() => setActiveSection("experience")}
+          >
+            Experience
+          </Card>
+          <Card
+            className={`w-3/4 px-4 py-3 ${
+              activeSection === "skills" ? "bg-orange" : ""
+            }`}
+            onClick={() => setActiveSection("skills")}
+          >
+            Professional Skills
+          </Card>
         </div>
-        <div>
-          <About />
-        </div>
+        <div className="w-2/3">{renderContent()}</div>
       </div>
     </div>
   );
---
+++ app/components/common/ProfessionalSkills.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const ProfessionalSkills = (props: Props) => {
+  return <div>ProfessionalSkills</div>;
+};
+
+export default ProfessionalSkills;
