e7739b0dd177beffc105645effa04eb80d9457b7
esthercate <vuthi.esther@gmail.com>
2024-08-28 13:55:50 +0300

    complete about section

--- app/components/common/About.tsx
+++ app/components/common/About.tsx
@@ -10,7 +10,7 @@ type Props = {};
 const About = (props: Props) => {
   return (
     <div className="flex flex-col gap-4">
-      <div className="w-full h-auto flex items-center gap-10">
+      <div className="w-full h-auto flex gap-10">
         <Profile className="h-36 w-36">
           <Image
             src="/profile.jpg"
@@ -37,7 +37,18 @@ const About = (props: Props) => {
               <p className="text-xs">www.catherine.com</p>
             </div>
           </div>
-          <div></div>
+          <div className="flex gap-5 mt-2">
+            <p className="flex gap-2 items-center text-sm">
+              <span className="text-orange text-sm font-semibold gap-3">
+                5+
+              </span>
+              Projects
+            </p>
+            <p className="flex gap-2 items-center text-xs">
+              <span className="text-orange text-sm font-semibold">2+</span>Years
+              of Experience
+            </p>
+          </div>
           <div className="w-2/5 mt-6">
             <Button className="py-3 text-sm">Download Resume</Button>
           </div>
