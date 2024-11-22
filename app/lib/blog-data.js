6d9375603a8292e97626869f16918f21dacd806c
esthercate <vuthi.esther@gmail.com>
2024-11-22 22:56:17 +0300

    Add blog images

--- app/lib/blog-data.js
+++ app/lib/blog-data.js
@@ -1,34 +1,57 @@
 export const blogPosts = [
   {
-    image: "/article1.jpg",
-    title: "How learning Javascript changed my career",
-    description: "Lorem ipsum dolor sit amet consectetur...",
+    image: "/img5.jpg",
+    title:
+      "Learning, Growing, and Thriving: Bootcamp to Software Guru",
+    description:
+      "When I finished my coding bootCamp 2 years ago, I was super excited to start my first job...",
     author: "Catherine Vuthi",
-    datePosted: "1w ago",
-    url: "/",
+    datePosted: "August 18, 2024",
+    url: "https://dev.to/catevee/learning-growing-and-thriving-my-journey-from-coding-bootcamp-to-software-guru-9e2",
   },
   {
-    image: "/article1.jpg",
-    title: "How learning Javascript changed my career",
-    description: "Lorem ipsum dolor sit amet consectetur...",
+    image: "/img6.jpg",
+    title: "Are Coding Bootcamps Worth It? My Experience!",
+    description:
+      "I won't lie! Being in a coding bootcamp was probably one the hardest, exhausting, and most...",
     author: "Catherine Vuthi",
-    datePosted: "1w ago",
-    url: "/",
+    datePosted: "Sep 12, 2022",
+    url: "https://dev.to/catevee/are-coding-bootcamps-worth-it-my-experience-pne",
   },
   {
-    image: "/article1.jpg",
-    title: "How learning Javascript changed my career",
-    description: "Lorem ipsum dolor sit amet consectetur...",
+    image: "/img3.png",
+    title: "Migrating from SQLite3 to PostgreSql Database - Heroku",
+    description:
+      "Are you planning to deploy your SQLite3 based backend to Heroku? If you answered...",
     author: "Catherine Vuthi",
-    datePosted: "1w ago",
-    url: "/",
+    datePosted: "July 4, 2022",
+    url: "https://dev.to/catevee/migrating-from-sqlite3-to-postgresql-database-heroku-h4l",
   },
   {
-    image: "/article1.jpg",
-    title: "How learning Javascript changed my career",
-    description: "Lorem ipsum dolor sit amet consectetur...",
+    image: "/img4.jpg",
+    title: "Setting up your first ReactJS & Firebase Auth project",
+    description:
+      "Nowadays, security is very important on websites and apps. Sometimes, developers make...",
     author: "Catherine Vuthi",
-    datePosted: "1w ago",
-    url: "/",
+    datePosted: "May 28, 2022",
+    url: "https://dev.to/catevee/setting-up-your-first-reactjs-firebase-auth-project-4i0j",
   },
+  // {
+  //   image: "/article1.jpg",
+  //   title: "Getting Started with Ruby - Beginner",
+  //   description:
+  //     "Four weeks ago I kicked off a new journey of learning Ruby, a backend programming language...",
+  //   author: "Catherine Vuthi",
+  //   datePosted: "July 13, 2022",
+  //   url: "https://dev.to/catevee/getting-started-with-ruby-beginner-f6l",
+  // },
+  // {
+  //   image: "/article1.jpg",
+  //   title: "Embed Responsive Maps To Your Website - Mapbox GL API",
+  //   description:
+  //     "Did you know that using maps on your website not only helps users find your location...",
+  //   author: "Catherine Vuthi",
+  //   datePosted: "Apr 24, 2022",
+  //   url: "https://dev.to/catevee/embed-responsive-maps-to-your-website-mapbox-gl-api-4cnb",
+  // },
 ];
